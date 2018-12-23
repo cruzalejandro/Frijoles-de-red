32bda70100f9a9ed914ae22fb3f260a2168f7f30
Edgard Alejandro <45920120+cruzalejandro@users.noreply.github.com>
2018-12-23 16:52:31 -0600

    Update main.cpp
    Test for Calculator

--- Pakuit/main.cpp
+++ Pakuit/main.cpp
@@ -1,25 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.cpp
- * Author: edgar
- *
- * Created on December 16, 2018, 1:03 PM
- */
-
-#include <cstdlib>
-#include <iostream>
+# include <iostream>
 using namespace std;
 
-/*
- * 
- */
-int main() {
-    std::cout<<"Pakuit Pakuit";
+int main()
+{
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator either + or - or * or /: ";
+    cin >> op;
+
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+
+    switch(op)
+    {
+        case '+':
+            cout << num1+num2;
+            break;
+
+        case '-':
+            cout << num1-num2;
+            break;
+
+        case '*':
+            cout << num1*num2;
+            break;
+
+        case '/':
+            cout << num1/num2;
+            break;
+
+        default:
+            // If the operator is other than +, -, * or /, error message is shown
+            cout << "Error! operator is not correct";
+            break;
+    }
+
     return 0;
 }
 
